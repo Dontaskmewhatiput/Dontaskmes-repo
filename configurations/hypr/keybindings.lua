c830d4ce713bfd5dacb50559743fb300b3c9f48f
Dontaskmewhatiput <MarcoMail25@proton.me>
2026-09-17 17:14:09 -0700

    Fix some errors hopefully

--- configurations/hypr/keybindings.lua
+++ configurations/hypr/keybindings.lua
@@ -7,10 +7,12 @@ local mainMod = "SUPER" -- Sets "Windows" key as main modifier
 hl.bind(mainMod .. " + Q", hl.dsp.exec_cmd(terminal))
 local closeWindowBind = hl.bind(mainMod .. " + C", hl.dsp.windows.close())
 -- closeWindowBind:set_enabled(false)
-hl.bind(mainMod .. " + M",
-  hl.dsp.exec_cmd("command -v hyprshutdown >/dev/null 2>&1 && hyprshutdown || hyprctl dispatch 'hl.dsp.exit()'"))
+--
+hl.bind(mainMod .. " + M", hl.dsp.exec_cmd([=[
+    command -v hyprshutdown >/dev/null 2>&1 && hyprshutdown || hyprctl dispatch exit
+]=]))
 hl.bind(mainMod .. " + E", hl.dsp.exec_cmd(fileManager))
-hl.bind(mainMod .. " + V", hl.dsp.window.float({ action = "toggle" }))
+hl.bind(mainMod .. " + V", hl.dsp.layout.toggle_float())
 hl.bind(mainMod .. " + R", hl.dsp.exec_cmd(menu))
 hl.bind(mainMod .. " + P", hl.dsp.window.pseudo())
 hl.bind(mainMod .. " + J", hl.dsp.layout("togglesplit")) -- dwindle only
