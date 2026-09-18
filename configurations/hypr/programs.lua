d21000e6ae202927daa001df45e95b5c449cc6b1
Dontaskmewhatiput <MarcoMail25@proton.me>
2026-09-17 18:20:38 -0700

    Idk a lot to document down

--- configurations/hypr/programs.lua
+++ configurations/hypr/programs.lua
@@ -3,10 +3,12 @@
 ---------------------
 
 -- Set programs that you use
-terminal = "alacritty"
-fileManager = "dolphin"
-menu        = "rofi -show drun"
-browser = "helium-browser"
+terminal      = "alacritty"
+fileManager   = "dolphin"
+menu          = "rofi -show drun"
+browser       = "helium-browser"
+main_browser  = "zen-twilight"
+game_launcher = "steam"
 
 -------------------
 ---- AUTOSTART ----
@@ -15,10 +17,8 @@ browser = "helium-browser"
 -- See https://wiki.hypr.land/Configuring/Basics/Autostart/
 
 -- Autostart necessary processes (like notifications daemons, status bars, etc.)
- hl.on("hyprland.start", function () 
-  hl.exec_cmd("gsettings set org.gnome.interface color-scheme 'prefer-dark'")
-  hl.exec_cmd("gsettings set org.gnome.interface gtk-theme 'adw-gtk3'")
- end)
+hl.on("hyprland.start", function()
+  hl.exec_cmd("gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'")
+  hl.exec_cmd("gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3'")
+end)
 hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
-
-
